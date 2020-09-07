9750bf7c806a80953cd67e4d748e64691126ea58
awright <awrightbus@gmail.com>
2020-09-07 13:34:11 -0500

    Updates

---
+++ precache-manifest.a10d92e3066e433d959f8a1523243416.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12fdc485081c3e8517844fd495f9a73b",
+    "url": "/face-rec-react/index.html"
+  },
+  {
+    "revision": "62642522ac3ecc251bd7",
+    "url": "/face-rec-react/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "84e2fbd9732edae8fce8",
+    "url": "/face-rec-react/static/css/main.ca60608e.chunk.css"
+  },
+  {
+    "revision": "62642522ac3ecc251bd7",
+    "url": "/face-rec-react/static/js/2.e5f3effd.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-rec-react/static/js/2.e5f3effd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84e2fbd9732edae8fce8",
+    "url": "/face-rec-react/static/js/main.296b885e.chunk.js"
+  },
+  {
+    "revision": "fb8a03f652449e7a256f",
+    "url": "/face-rec-react/static/js/runtime-main.c28bd21c.js"
+  },
+  {
+    "revision": "fbf920375a7585a12c22a63abb89c1f6",
+    "url": "/face-rec-react/static/media/face.fbf92037.png"
+  }
+]);
